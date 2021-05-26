7644a4dabd1ffef261c5e7d2e6701bc0314269da
Huy <huy.tran@hotmail.com>
2021-05-25 23:00:03 -0400

    Completed 2e exercises

--- part1/src/Part2d.js
+++ part1/src/Part2d.js
@@ -3,8 +3,8 @@ import contactsService from './Services/contactsService'
 
 const Part2d = () => {
   const [ persons, setPersons ] = useState([]);
-
   const [ searchCriteria, setSearchCriteria ] = useState(''); 
+  const [ notification, setNotification ] = useState({message : null, isError : false});
 
   const contactsToShow = searchCriteria === '' ? persons : persons.filter(person => person.name.toLowerCase().startsWith(searchCriteria.trim().toLowerCase()));
 
@@ -24,11 +24,12 @@ const Part2d = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={notification.message} isError={notification.isError} displayMessage={setNotification}></Notification>
       <SearchForm criteria={searchCriteria} updateSearch={updateSearch}></SearchForm>
       <h3>Add New Contact</h3>
-      <PersonForm persons={persons} updatePersons={updatePersons} />
+      <PersonForm persons={persons} updatePersons={updatePersons} displayMessage={setNotification} />
       <h2>Numbers</h2>
-      <Persons contactsToShow={contactsToShow} updatePersons={updatePersons}></Persons>
+      <Persons contactsToShow={contactsToShow} updatePersons={updatePersons} displayMessage={setNotification}></Persons>
     </div>
   )
 }
@@ -41,7 +42,7 @@ const SearchForm = ({criteria, updateSearch}) => {
    )
 }
 
-const PersonForm = ({persons, updatePersons}) => {
+const PersonForm = ({persons, updatePersons, displayMessage}) => {
    const [ newName, setNewName ] = useState('');
    const [ newPhone, setNewPhone ] = useState('');
 
@@ -54,7 +55,14 @@ const PersonForm = ({persons, updatePersons}) => {
             if (window.confirm(`${newName} is already added to the phonebook. Replace the old number with the new one?`)) {
                 contactsService.updateContact(contact.id, {...contact, phone : newPhone})
                     .then(returnedContact => {
-                        updatePersons(persons.map(p => p.id !== contact.id ? p : returnedContact));
+                        updatePersons(persons.map(p => p.id !== contact.id ? p : returnedContact));                       
+                        displayMessage({ message : `The phone number for contact ${newName} was successfully updated`, isError : false });
+                    })
+                    .catch(err => {
+                        contactsService.getContacts().then(contacts => updatePersons(contacts));
+                        displayMessage({ message : `Contact ${newName} has already been removed from server`, isError : true });
+                    })
+                    .finally(() => {
                         setNewName('');
                         setNewPhone('');
                     })
@@ -72,6 +80,7 @@ const PersonForm = ({persons, updatePersons}) => {
                 updatePersons(persons.concat(newContact));
                 setNewName('');
                 setNewPhone('');
+                displayMessage({ message : `${newName} was successfully added to the phonebook`, isError : false });
             })
     }   
 
@@ -90,13 +99,17 @@ const PersonForm = ({persons, updatePersons}) => {
    )
 }
 
-const Persons = ({contactsToShow, updatePersons}) => {
+const Persons = ({contactsToShow, updatePersons, displayMessage}) => {
     const deletePerson = (id, name) => {
         if (window.confirm(`Do you wish to delete ${name}?`)) {
             contactsService.deleteContact(id)
-                .then(() => {
-                    updatePersons(contactsToShow.filter(contact => contact.id !== id));
+                .then(() => {                   
+                    displayMessage({ message : `${name} was successfully deleted`, isError : false });
                 })
+                .catch(err => {
+                    displayMessage({ message : `Contact ${name} has already been removed from server`, isError : true});
+                })
+                .finally(() => updatePersons(contactsToShow.filter(contact => contact.id !== id)));
         }
    }
    
@@ -116,6 +129,40 @@ const Person = ({name, phone, deletePerson}) => {
        <>
            <li style={{paddingBottom:'5px'}}>{name}: {phone} <button onClick={deletePerson}>DELETE</button></li>
        </>
+   )
+}
+
+const Notification = ({message, isError, displayMessage}) => {
+   const error = {
+        color: 'red',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px'
+   }
+
+   const info = {
+        color: 'green',
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px'
+   }
+
+   if (message == null) return null;
+
+   setTimeout(() => {
+       displayMessage({ message: null });
+   }, 3000);
+
+   return (
+       <div id='alert' style={isError ? error : info}>
+           {message}
+       </div>
    )
 }
 
